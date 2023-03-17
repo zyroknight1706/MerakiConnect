5b5180d028d89e019a1844806359d42e400f1296
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2023-03-17 22:39:48 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    margin: 0;
+    padding: 0;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Logo></Logo>
+      
+    </Container>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../Components/Navbar.jsx'
+
+const Home = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default Home
